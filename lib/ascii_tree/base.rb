c887bb91389b565b13124490fab7911de99162c2
Chris Patuzzo <chris@patuzzo.co.uk>
2015-01-31 18:35:22 +0000

    Get the ‘Usage’ example passing

--- lib/ascii_tree/base.rb
+++ lib/ascii_tree/base.rb
@@ -1,2 +1,10 @@
 module AsciiTree
+  def self.parse(string)
+    string        = CommentStripper.strip(string)
+    words         = WordParser.parse(string)
+    edges         = EdgeParser.parse(string)
+    relationships = RelationshipsBuilder.build(words, edges)
+
+    NodeBuilder.build(relationships)
+  end
 end
